503ea52161b85ab3c2f8fba11a59bd2e264f6096
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-12-31 05:23:33 +0530

    add server files with FastAPI setup and .gitignore

---
+++ server/main.py
@@ -0,0 +1,18 @@
+from typing import Union
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+
+)
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello from Server"}
